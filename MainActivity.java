276965df25fa0842c61428c9cff71db3b5dd4f3e
BohdanSushchak <bogdansushak@gmail.com>
2017-10-03 14:34:17 +0200

    Initial commit

---
+++ MainActivity.java
@@ -0,0 +1,14 @@
+package sushchak.bohdan.curabitur;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private static String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
