89fdae51a59e6a7034de4e2550aea52243ca30a4
BohdanSushchak <bogdansushak@gmail.com>
2017-10-18 18:32:02 +0200

    Created a class for manage image.

---
+++ utils/ImageUtils.java
@@ -0,0 +1,52 @@
+package sushchak.bohdan.curabitur.utils;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.net.Uri;
+import android.provider.MediaStore;
+
+import java.io.ByteArrayOutputStream;
+
+public class ImageUtils {
+
+    public static final int USER_AVATAR_WIDTH = 128;
+    public static final int USER_AVATAR_HEIGH = 128;
+
+    public static Uri getImageUri(Context inContext, Bitmap inImage) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        String path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, "Title", null);
+        return Uri.parse(path);
+    }
+
+    public static Bitmap cropToSquare(Bitmap srcBmp) {
+        Bitmap dstBmp = null;
+        if (srcBmp.getWidth() >= srcBmp.getHeight()) {
+
+            dstBmp = Bitmap.createBitmap(
+                    srcBmp,
+                    srcBmp.getWidth() / 2 - srcBmp.getHeight() / 2,
+                    0,
+                    srcBmp.getHeight(),
+                    srcBmp.getHeight()
+            );
+
+        } else {
+            dstBmp = Bitmap.createBitmap(
+                    srcBmp,
+                    0,
+                    srcBmp.getHeight() / 2 - srcBmp.getWidth() / 2,
+                    srcBmp.getWidth(),
+                    srcBmp.getWidth()
+            );
+        }
+
+        return dstBmp;
+    }
+
+}
